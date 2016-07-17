7b6cd17422e1cad63654051e8e1f6ed0dbc9ee28
janfb <j.f.boelts@gmail.com>
2016-07-17 22:43:02 +0200

    New test for mapping postions to states

--- test_maze.py
+++ test_maze.py
@@ -9,8 +9,15 @@ class Test_wetter_com(unittest.TestCase):
 
     def test_geometry(self):
         print("Testing geometry")
-        self.maze.visualize_maze(plot=False)
+        self.maze.visualize_maze(plot=True)
 
     def test_discretization(self):
         print("Testing discretization")
         assert(np.mod(self.maze.Nstates,10)==0)
+
+    def test_statemapping(self):
+        for i in range(100):
+            self.maze.x_position = np.random.uniform(low=0, high=2*self.maze.armX+self.maze.armY)
+            self.maze.y_position = np.random.uniform(low=0, high=self.maze.armX+self.maze.armY)
+            self.maze.action=0
+            self.maze._update_state()
